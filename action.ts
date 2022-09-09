ffce5f57822cf4cbfa666b5b251d849895e55bd3
ren <ren.soharab@quicksave.fi>
2022-09-09 23:51:55 +0300

    clean up action

--- action.ts
+++ action.ts
@@ -1,36 +1,4 @@
 import axios from 'axios'
-import useSWR from 'swr'
-import { useStore } from './store'
-import * as api from './types'
-
-const { dispatch }: types.Dispatch  = useStore()
 
 const get = (url: string) => axios.get(url).then(res => res.data)
 const post = (url: string, params: any) => axios.post(url, {...params}).then(res => res.data)
-
-export const useGetEvents = () => {
-    const { data, error } = useSWR(process.env.API_URL, get)
-
-    return {
-
-    }
-}
-
-export const useGetSessions = () => {
-    const { data, error } = useSWR(process.env.API_URL, get)
-}
-
-export const useGetQuestions = () => {
-    const { data, error } = useSWR(process.env.API_URL, get)
-}
-
-export const storeQuestions = () => {
-    
-    const params = {}
-    const { data, error } = useSWR(process.env.API_URL, post(params))
-}
-
-export const storeQuestionVote = () => {
-    const params = {}
-    const { data, error } = useSWR(process.env.API_URL, post(params))
-}
