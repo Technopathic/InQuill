55155e6a58c273f634696c612e797b40a806d188
ren <ren.soharab@quicksave.fi>
2022-09-11 19:01:30 +0300

    feat(client): Adds author to question

--- pages/[slug]/[sessionSlug]/index.tsx
+++ pages/[slug]/[sessionSlug]/index.tsx
@@ -14,7 +14,7 @@ const Questions: NextPage<types.QuestionsPage> = (props) => {
   const { session, questions }: types.State = useStore()
   const { dispatch }: types.Dispatch = useStore()
   const questionRef = useRef<HTMLTextAreaElement>(null)
-  
+  const authorRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     if(props.error) {
@@ -32,7 +32,7 @@ const Questions: NextPage<types.QuestionsPage> = (props) => {
 
   const handleStoreQuestion = async() => {
     if(questionRef.current) {
-      const response = await storeQuestion(session.slug, questionRef.current.value, 'Anonymous')
+      const response = await storeQuestion(session.slug, questionRef.current.value, authorRef.current?.value || '')
       if(response.error) {
         dispatch({ type: 'SET_SNACK', value: { show: true, message: response.error }})
       } else {
@@ -45,11 +45,16 @@ const Questions: NextPage<types.QuestionsPage> = (props) => {
   return (
     <>
       <header className="w-full flex flex-col justify-center items-center">
-          <div className="flex flex-col justify-center items-center w-full py-8">
+          <div className="flex flex-col justify-center max-w-screen-sm items-center w-full pt-8 pb-6">
               <h1 className="text-slate-700 text-2xl text-center">{session.title}</h1>
-              <div className="w-full max-w-screen-sm mt-4">
+              <div className="flex justify-between w-full">
+                <span>{session.speaker}</span>
+                <span></span>
+              </div>
+              <div className="w-full mt-4">
                 <textarea className="w-full h-32 p-2 resize-none rounded-xl" ref={questionRef} placeholder="Ask your question here"></textarea>
-                <div className="flex justify-end mt-1">
+                <div className="flex justify-between mt-1">
+                  <input className="flex rounded-xl w-1/2 px-3" placeholder="Your name (optional)" ref={authorRef} />
                   <div className="cursor-pointer bg-slate-800 py-2 px-10 rounded-xl text-white text-xl select-none" onClick={handleStoreQuestion}>Ask</div>
                 </div>
               </div>
