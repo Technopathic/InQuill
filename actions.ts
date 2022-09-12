d1124137c59eef04270a4da8e6a8f165f86b0665
ren <ren.soharab@quicksave.fi>
2022-09-12 17:18:37 +0300

    Corrected async function in actions

--- actions.ts
+++ actions.ts
@@ -70,7 +70,7 @@ export const signInWithGoogle = async() => {
     return { user, session, error }
 }
 
-export const getUser = async() => {
+export const getUser = () => {
     const user = supabase.auth.user()
     console.log(user)
     return user
