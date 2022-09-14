2b8f61cfd4058e2e2ae9dd56cede9200a6d8cc14
ren <ren.soharab@quicksave.fi>
2022-09-14 14:49:37 +0300

    Fixes footer link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: NextPage = () => {
         <footer className="w-full h-10 bg-slate-800 flex justify-center">
             <div className="w-full max-w-screen-sm text-white px-2 justify-between items-center flex">
                 <span className="text-sm">Built with ❤️ by 
-                    <a href="https://github.com/Techhnopathic/InQuill">Technopathic</a>
+                    <a className="ml-1" href="https://github.com/Technopathic/InQuill">Technopathic</a>
                 </span>
                 <span className="text-sm">
                     <Link href="/tos" passHref><a className="ml-2">Terms of Service</a></Link>
