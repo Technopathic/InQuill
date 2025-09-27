5bb72c6faa74e55542319f75dd18f7c3c0c529b7
Ren <nsoharab@gmail.com>
2025-09-27 22:05:17 +0300

    Disable check-in

--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -11,11 +11,11 @@ const Event: NextPage<types.EventPage> = (props) => {
         <h1 className={`text-8xl siteTitle`}>{props.event.title}</h1>
       </section>
       <section className="grid grid-cols-2 gap-8 w-full max-w-md mt-4">
-        <Link href={`${props.event.slug}/check-in`} passHref>
+        {/*<Link href={`${props.event.slug}/check-in`} passHref>
           <a className="bg-slate-700 text-white text-2xl text-center rounded-lg py-2">
             Check-In
           </a>
-        </Link>
+        </Link>*/}
         <Link href={`${props.event.slug}/sessions`} passHref>
           <a className="bg-slate-700 text-white text-2xl text-center rounded-lg py-2">
             Q & A
