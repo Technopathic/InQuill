b13202e7f6e50738ea9b570399786a8c1d79b6f2
Ren <nsoharab@gmail.com>
2025-10-02 17:23:02 +0300

    Add QR codes

---
+++ pages/qr_ld25.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
+import QRCode from "react-qr-code"
+
+const QRCodeLD25 = () => {
+    return (
+        <main className="flex flex-col justify-between h-screen blueBackground">
+            <div className="flex pl-8 pt-8 pr-8 items-center">
+                <div className="mr-8">
+                    <img className="h-20" src="/IGDALogo.png" alt="IGDA LOGO" />
+                </div>
+                <div className="flex flex-col mt-2">
+                    <h1 className="text-4xl font-bold text-white">Leadership Day 2025 by IGDA Finland</h1>
+                    <p className="text-2xl text-white">Helsinki City Hall 06.10.2025</p>
+                </div>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <h2 className="text-6xl font-bold mb-4 text-white">Event Registration</h2>
+                <div className="h-96 w-96 p-3">
+                    <QRCode
+                        size={256}
+                        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                        value="https://luma.com/y7mpolc6" 
+                    />
+                </div>
+                <h3 className="mt-8 text-white text-3xl px-8">Scan this if you do not have a ticket</h3>
+            </div>
+            <div></div>
+        </main>
+    )
+}
+
+export default QRCodeLD25
---
+++ pages/qr_ldmixer25.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
+import QRCode from "react-qr-code"
+
+const QRCodeLDMixer25 = () => {
+    return (
+        <main className="flex flex-col justify-between h-screen blueBackground">
+            <div className="flex pl-8 pt-8 pr-8 items-center">
+                <div className="flex flex-col mt-2">
+                    <h1 className="text-4xl font-bold text-white">Leadership Day Mixer 2025</h1>
+                    <p className="text-2xl text-white">Sofiankatu 4C 06.10.2025</p>
+                </div>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <h2 className="text-6xl font-bold mb-4 text-white">Event Registration</h2>
+                <div className="h-96 w-96 p-3">
+                    <QRCode
+                        size={256}
+                        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                        value="https://luma.com/nyn8jrue" 
+                    />
+                </div>
+                <h3 className="mt-8 text-white text-3xl px-8">Scan this if you do not have a ticket</h3>
+            </div>
+            <div></div>
+        </main>
+    )
+}
+
+export default QRCodeLDMixer25
