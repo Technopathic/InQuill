8c7161cc11d93e56bbef9841b75bef382644faa7
Ren <nsoharab@gmail.com>
2025-10-04 21:46:27 +0300

    Change colors

--- pages/qr_ld25.tsx
+++ pages/qr_ld25.tsx
@@ -3,18 +3,18 @@ import QRCode from "react-qr-code"
 
 const QRCodeLD25 = () => {
     return (
-        <main className="flex flex-col justify-between h-screen blueBackground">
+        <main className="flex flex-col justify-between h-screen bg-white">
             <div className="flex pl-8 pt-8 pr-8 items-center">
                 <div className="mr-8">
                     <img className="h-20" src="/IGDALogo.png" alt="IGDA LOGO" />
                 </div>
                 <div className="flex flex-col mt-2">
-                    <h1 className="text-4xl font-bold text-white">Leadership Day 2025 by IGDA Finland</h1>
-                    <p className="text-2xl text-white">Helsinki City Hall 06.10.2025</p>
+                    <h1 className="text-4xl font-bold text-black">Leadership Day 2025 by IGDA Finland</h1>
+                    <p className="text-2xl text-black">Helsinki City Hall 06.10.2025</p>
                 </div>
             </div>
             <div className="flex flex-col justify-center items-center">
-                <h2 className="text-6xl font-bold mb-4 text-white">Event Registration</h2>
+                <h2 className="text-6xl font-bold mb-4 text-black">Event Registration</h2>
                 <div className="h-96 w-96 p-3">
                     <QRCode
                         size={256}
@@ -22,7 +22,7 @@ const QRCodeLD25 = () => {
                         value="https://luma.com/y7mpolc6" 
                     />
                 </div>
-                <h3 className="mt-8 text-white text-3xl px-8">Scan this if you do not have a ticket</h3>
+                <h3 className="mt-8 text-black text-3xl px-8">Scan this if you do not have a ticket</h3>
             </div>
             <div></div>
         </main>
--- pages/qr_ldmixer25.tsx
+++ pages/qr_ldmixer25.tsx
@@ -3,15 +3,15 @@ import QRCode from "react-qr-code"
 
 const QRCodeLDMixer25 = () => {
     return (
-        <main className="flex flex-col justify-between h-screen blueBackground">
+        <main className="flex flex-col justify-between h-screen bg-white">
             <div className="flex pl-8 pt-8 pr-8 items-center">
                 <div className="flex flex-col mt-2">
-                    <h1 className="text-4xl font-bold text-white">Leadership Day Mixer 2025</h1>
-                    <p className="text-2xl text-white">Sofiankatu 4C 06.10.2025</p>
+                    <h1 className="text-4xl font-bold text-black">Leadership Day Mixer 2025</h1>
+                    <p className="text-2xl text-black">Sofiankatu 4C 06.10.2025</p>
                 </div>
             </div>
             <div className="flex flex-col justify-center items-center">
-                <h2 className="text-6xl font-bold mb-4 text-white">Event Registration</h2>
+                <h2 className="text-6xl font-bold mb-4 text-black">Event Registration</h2>
                 <div className="h-96 w-96 p-3">
                     <QRCode
                         size={256}
@@ -19,7 +19,7 @@ const QRCodeLDMixer25 = () => {
                         value="https://luma.com/nyn8jrue" 
                     />
                 </div>
-                <h3 className="mt-8 text-white text-3xl px-8">Scan this if you do not have a ticket</h3>
+                <h3 className="mt-8 text-black text-3xl px-8">Scan this if you do not have a ticket</h3>
             </div>
             <div></div>
         </main>
